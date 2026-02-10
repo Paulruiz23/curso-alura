c1aca3f2dcec35a45a570baca7c2e8570d0b37ce
Paul Ruiz <Paul_106_6@hotmail.com>
2026-02-09 20:55:18 -0500

    Añade practicas creando API y conexion a FronEnd

---
+++ 4.backend-java/2.Java-y-Spring-Framework/Spring4_Primera_Api/screenmatch/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.aluracursos.screenmatch.model;
+
+public enum Categoria {
+    CRIMEN("Crime","Crimen"),
+    ACCION("Action","Acción"),
+    ROMANCE("Romance","Romance"),
+    COMEDIA("Comedy","Comedia"),
+    DRAMA("Drama","Drama"),
+    AVENTURA("Adventure","Aventura");
+
+    private String categoriaEspanol;
+    private String categoriaOmdb;
+
+    Categoria (String categoriaOmdb,String categoriaEspanol){
+        this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEspanol = categoriaEspanol;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+
+    public static Categoria fromEspanol(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaEspanol.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+}
