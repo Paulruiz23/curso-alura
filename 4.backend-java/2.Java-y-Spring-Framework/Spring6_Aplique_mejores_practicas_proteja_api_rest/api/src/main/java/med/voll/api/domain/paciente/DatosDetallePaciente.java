c055b0ec063357a4974be223282f347ee667502d
Paul Ruiz <Paul_106_6@hotmail.com>
2026-02-18 18:10:02 -0500

    Agrega practicas

---
+++ 4.backend-java/2.Java-y-Spring-Framework/Spring6_Aplique_mejores_practicas_proteja_api_rest/api/src/main/java/med/voll/api/domain/paciente/DatosDetallePaciente.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.paciente;
+
+import med.voll.api.domain.direccion.Direccion;
+
+public record DatosDetallePaciente(
+        String nombre,
+        String email,
+        String telefono,
+        String documentoIdentidad,
+        Direccion direccion
+) {
+
+    public DatosDetallePaciente(Paciente paciente) {
+        this(
+                paciente.getNombre(),
+                paciente.getEmail(),
+                paciente.getTelefono(),
+                paciente.getDocumentoIdentidad(),
+                paciente.getDireccion());
+    }
+}
---
+++ 4.backend-java/2.Java-y-Spring-Framework/Spring6_Aplique_mejores_practicas_proteja_api_rest/api/src/main/java/med/voll/api/domain/paciente/DatosRegistroPaciente.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.domain.direccion.DatosDireccion;
+
+public record DatosRegistroPaciente(
+    @NotBlank String nombre,
+    @NotBlank @Email String email,
+    @NotBlank @Pattern(regexp = "\\d{7,9}") String documento_identidad,
+    @NotBlank String telefono,
+    @NotNull @Valid DatosDireccion direccion) {
+
+}
